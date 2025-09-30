187805bf1df537b920815802da922159746b63f1
andrewor14 <andrewor14@gmail.com>
2025-09-30 14:42:20 -0700

    Generalize parse script to bbh and mmlu_pro

--- parse_it.py
+++ parse_it.py
@@ -10,13 +10,38 @@ if len(sys.argv) == 1:
     sys.exit(1)
 log_dirs = sys.argv[1:]
 
+PREFIX_TO_CUT = os.getenv("PREFIX_TO_CUT", "")
+
 def extract_wikitext_perplexity(log_file: str) -> float:
     with open(log_file, "r") as f:
         for l in f.readlines():
             m = re.match(".*\|word_perplexity\|↓  \|([\\d.]*)\|.*", l)
             if m is not None:
                 return float(m.groups()[0])
-    raise ValueError("Did not find wikitext perplexity in %s" % log_file)
+    return -1
+
+def extract_bbh_accuracy(log_file: str) -> float:
+    # |bbh     |      3|get-answer    |      |exact_match|↑  |0.7068|±  |0.0050|
+    with open(log_file, "r") as f:
+        for l in f.readlines():
+            m = re.match("\|bbh.*\|([\\d.]*)\|±.*", l)
+            if m is not None:
+                return float(m.groups()[0])
+    return -1
+
+def extract_mmlu_pro_accuracy(log_file: str) -> float:
+    with open(log_file, "r") as f:
+        for l in f.readlines():
+            m = re.match("\|mmlu_pro.*\|([\\d.]*)\|±.*", l)
+            if m is not None:
+                return float(m.groups()[0])
+    return -1
+
+task_to_parse_fn = {
+    "wikitext": extract_wikitext_perplexity,
+    "bbh": extract_bbh_accuracy,
+    "mmlu_pro": extract_mmlu_pro_accuracy,
+}
 
 for log_dir in log_dirs:
     if "qat" not in log_dir:
@@ -36,24 +61,32 @@ for log_dir in log_dirs:
             all_data[experiment_name] = {}
             float_eval_file = os.path.join(experiment_dir, "eval_float.log")
             quantized_eval_file = os.path.join(experiment_dir, "eval_quantized.log")
-            all_data[experiment_name]["wikitext_word_perplexity_float"] = extract_wikitext_perplexity(float_eval_file)
-            all_data[experiment_name]["wikitext_word_perplexity_quantized"] = extract_wikitext_perplexity(quantized_eval_file)
+            for task, parse_fn in task_to_parse_fn.items():
+                all_data[experiment_name][f"{task}_float"] = parse_fn(float_eval_file)
+                all_data[experiment_name][f"{task}_quantized"] = parse_fn(quantized_eval_file)
     except FileNotFoundError:
         print(f"Skipping log directory {log_dir}")
         continue
 
     # print data in a nice format
-    metric = "wikitext_word_perplexity"
-    baseline_float_value = all_data[baseline_name][metric + "_float"]
-    baseline_quantized_value = all_data[baseline_name][metric + "_quantized"]
-    qat_quantized_value = all_data[qat_name][metric + "_quantized"]
-    recovered = (qat_quantized_value - baseline_quantized_value) / (baseline_float_value - baseline_quantized_value) * 100
-    print(
-        "%s: %.3f (baseline) -> %.3f (quant) -> %.3f (qat), recovered %.3f%%" % (
-            log_dir.split("/")[-1].replace("Llama3.2-3B_", ""),
-            baseline_float_value,
-            baseline_quantized_value,
-            qat_quantized_value,
-            recovered,
+    for task in task_to_parse_fn.keys():
+        baseline_float_value = all_data[baseline_name][task + "_float"]
+        baseline_quantized_value = all_data[baseline_name][task + "_quantized"]
+        qat_quantized_value = all_data[qat_name][task + "_quantized"]
+        if baseline_float_value == -1 or baseline_quantized_value == -1:
+            print(
+                f"Warning: baseline float value for task {task} was {baseline_float_value}, "
+                f"quantized value was {baseline_quantized_value}"
+            )
+            continue
+        recovered = (qat_quantized_value - baseline_quantized_value) / (baseline_float_value - baseline_quantized_value) * 100
+        print(
+            "%s: %s (baseline) -> %s (quant) -> %s (qat), recovered %.3f%% (%s)" % (
+                log_dir.split("/")[-1].replace(PREFIX_TO_CUT, ""),
+                baseline_float_value,
+                baseline_quantized_value,
+                qat_quantized_value,
+                recovered,
+                task,
+            )
         )
-    )
